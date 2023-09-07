9994858d4a325b445869f0f1bd8f842291ceea46
Lorenzo Ricciardiello <lorenzo1995111@gmail.com>
2023-09-07 01:07:22 +0100

    editFinish

--- resources/views/livewire/song-index.blade.php
+++ resources/views/livewire/song-index.blade.php
@@ -4,8 +4,8 @@
 <section class="container mt-5" >
     <div class="row" >
     @foreach($songs as $song)
-    <div class="col-12 col-md-3 my-3 bordi">
-        <div class="card" style="width: 18rem;height: 28rem;">
+    <div class="col-12 col-md-3 my-5">
+        <div class="card">
         <div class="card-body text-center">
             <h5 class="card-title">{{$song->name}}</h5>
             <p class="card-text">{{$song->id}}</p>
@@ -13,8 +13,8 @@
             <p class="card-text">{{$song->price}}€</p>
             <p class="card-text">{{$song->category}}</p>
         </div>
-        <a href="{{ route('song.edit', $song) }}" class="btn btn-secondary" >Modifica</a>
-        <button class="btn btn-danger" wire:click="delete({{ $song->id }})">Cancella</button>
+        <a href="{{ route('song.edit', $song) }}" class="btn btn-secondary bottoni" >Modifica</a>
+        <button class="btn btn-danger bottoni" wire:click="delete({{ $song->id }})">Cancella</button>
     </div>
     </div>
     @endforeach
